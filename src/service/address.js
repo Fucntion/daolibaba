e73616a28aa9800202b0822742be34da4caa3b3c
zijian <a159qaz951zaq@gmail.com>
2018-05-13 22:14:30 +0800

    把登录获取openid的问题留给订单页面吧
    如题

--- src/service/address.js
+++
@@ -1,9 +0,0 @@
-export const provinces = require('./provinces');
-export const citys = require('./cities');
-export const areas = require('./areas');
-
-export default {
-  provinces,
-  citys,
-  areas
-}
